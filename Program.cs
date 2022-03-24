90a6644c92805a2dc5cf4dc36b7f9f9bed00b608
Santiago Inwinkelried <inwin18@gmail.com>
2022-03-24 20:38:05 -0300

    creando repositorio Practica2

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //EJERCICIO1
+            //Console.WriteLine("ingrese el primer numero");
+            //int numero1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("ingrese el segundo numero");
+            //int numero2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("ingrese el tercer numero");
+            //int numero3 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("ingrese el cuatro numero");
+            //int numero4 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("ingrese el quinto numero");
+            //int numero5 = int.Parse(Console.ReadLine());
+
+            //double prom = (numero1 + numero2 + numero3 + numero4 + numero5) / 5;
+            //Console.WriteLine("el promedio es: "+prom);
+            //Console.ReadLine();
+
+            //EJERCICIO2
+
+            //Console.WriteLine("ingresar un numero mayor a cero:");
+            //double numero = double.Parse(Console.ReadLine());
+            //if (numero > 0)  
+            //    Console.WriteLine(Math.Pow( numero,  2));
+            //else
+            //    Console.WriteLine("Error. Numero incorrecto");
+            //Console.ReadKey();
+
+            //EJERCICIO3
+
+            //const double pi = 3.1416;
+            //Console.WriteLine("Ingrese el radio del circulo: ");
+            //double radio = double.Parse(Console.ReadLine());
+            //double circunferencia = pi * radio * 2;
+            //double area = pi * Math.Pow(radio, 2);
+            //Console.WriteLine("El perimetro de la circunferencia es:" + circunferencia);
+            //Console.WriteLine("El area de la circunferencia es: " + area);
+            //Console.ReadKey();
+
+            //EJERCICIO4
+
+            //Console.WriteLine("ingrese un numero:");
+            //int numero = int.Parse(Console.ReadLine());
+            //if (numero >= 0)
+            //    Console.WriteLine("numero positivo");
+            //else 
+            //    Console.WriteLine("numero negativo");
+            //Console.ReadKey();
+
+            //EJERCICIO5 
+
+            //Console.WriteLine("Ingrese un numero");
+            //int numero = int.Parse(Console.ReadLine());
+            //if (numero % 2 == 0)
+            //    Console.WriteLine("numero par");
+            //else
+            //    Console.WriteLine("numero impar");
+            //Console.ReadKey();
+
+            //EJERCICIO6
+
+            //Console.WriteLine("Ingrese grados celsius");
+            //const double factor = 2.12;
+            //double celsius = double.Parse(Console.ReadLine());
+            //double farenheit = celsius * factor;
+            //Console.WriteLine("El valor ingresado equivale a "+ farenheit + " grados farenheit");
+            //Console.ReadKey();
+
+            //EJERCICIO7
+
+            //Console.WriteLine("ingrese un valor en centimetros");
+            //double centimetros= double.Parse(Console.ReadLine());
+            //const double factorPulgadas = 0.393701;
+            //const double factorPies = 0.0328084;
+            //Console.WriteLine("Su valor convertido de centimetros a pies es: " + centimetros * factorPies);
+            //Console.WriteLine("Su valor convertido de centimetros a pulgadas es: " + centimetros * factorPulgadas);
+            //Console.ReadKey();
+
+            //EJERCICIO8
+                
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
